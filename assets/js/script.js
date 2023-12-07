da5185504aa078624c1c4383f7ef3b043be34f70
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2023-12-07 02:19:11 -0300

    Adição de 'Gênero'
    Adição de 'Gênero' ao resumo do formulário, após "enviar".

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,21 +15,43 @@ function escopo (){
     function getFormulario(evento){
         evento.preventDefault();
 
-        // Input's de gênero e de foto serão adicionados em breve!
-        const gen1 = formulario.querySelector('#gen1');
-        const gen2 = formulario.querySelector('#gen2');
-        const gen3 = formulario.querySelector('#gen3');
+        // Input de foto será adicionado em breve!
+
         const foto = formulario.querySelector('#arquivo');
+
         const nascimento = formulario.querySelector('#nascimento');
+
         const correio = formulario.querySelector('#correio');
+
         const senha = formulario.querySelector('#senha');
+
         const cor = formulario.querySelector('#cor');
+
         const red = formulario.querySelector('#red');
         const green = formulario.querySelector('#green');
         const blue = formulario.querySelector('#blue');
 
+        // selecionaGenero: Confere qual gênero foi escolhido para enviar na resposta do formulário
+        function selecionaGenero(){
+            const gen1 = document.getElementById('gen1');
+            const gen2 = document.getElementById('gen2');
+            const genx = document.getElementById('genx');
+
+            let temporario = '';
+            if (gen1.checked){
+                temporario = 'Masculino';
+            }
+            else if (gen2.checked) {
+                temporario = 'Feminino';
+            }
+            else if (genx.checked){
+                temporario = 'Outr@';
+            }
+            return temporario;
+        }
+
         // addFormulario: Utilizado para gravar os resultados na lista completa de envios
-        function addFormulario(a, b, c, d, e, f, g){
+        function addFormulario(a, b, c, d, e, f, g, h){
             const resultado = {
                 nascimento : a,
                 correio : b,
@@ -38,8 +60,10 @@ function escopo (){
                 red : e,
                 green : f,
                 blue : g,
+                genero : h,
+
                 alerta (){
-                    alert(`Formulário de '${this.correio}' enviado!`);
+                    alert(`Formulário de " ${this.correio} " enviado!`);
                 }
             };
             resultado.alerta();
@@ -47,7 +71,7 @@ function escopo (){
         }
 
         // resumo: Atualiza a tabela ao fim do formulário com as respostas dadas pelo usuário
-        function resumo(a, b, c, d, e, f, g){
+        function resumo(a, b, c, d, e, f, g, h){
             const nascimento = document.getElementById('a');
             const correio = document.getElementById('b');
             const senha = document.getElementById('c');
@@ -55,6 +79,7 @@ function escopo (){
             const red = document.getElementById('e');
             const green = document.getElementById('f');
             const blue = document.getElementById('g');
+            const genero = document.getElementById('h');
 
             nascimento.innerHTML = a;
             correio.innerHTML = b;
@@ -63,11 +88,13 @@ function escopo (){
             red.innerHTML = e;
             green.innerHTML = f;
             blue.innerHTML = g;
+            genero.innerHTML = h;
+
         }
 
-        formularios.push(addFormulario(nascimento.value, correio.value, senha.value, cor.value, red.value, green.value, blue.value));
+        formularios.push(addFormulario(nascimento.value, correio.value, senha.value, cor.value, red.value, green.value, blue.value, selecionaGenero()));
 
-        resumo(nascimento.value, correio.value, senha.value, cor.value, red.value, green.value, blue.value);
+        resumo(nascimento.value, correio.value, senha.value, cor.value, red.value, green.value, blue.value, selecionaGenero());
 
         console.log(`Formulário de n°: ${contador}`);
         contador++;
